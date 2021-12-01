752c50c1c07a7a551a4838e620be10259d853f6e
MagedSaeed <msmj2015@gmail.com>
2021-12-01 17:20:23 +0300

    fix bug to predict chars instead of meter labels

--- fine-tuning/arg_parsers.py
+++ fine-tuning/arg_parsers.py
@@ -166,26 +166,28 @@ class DataCollatorCTCWithPadding:
         input_features = [
             {"input_values": feature["input_values"]} for feature in features
         ]
-
-        def onehot(lbl):
-            print(lbl)
-            onehot = [0] * 16
-            onehot[int(lbl)] = 1
-            return onehot
-
-        output_features = [onehot(feature["labels"]) for feature in features]
+        label_features = [{"input_ids": feature["labels"]} for feature in features]
 
         batch = self.processor.pad(
             input_features,
-            padding=True,
+            padding=self.padding,
             max_length=self.max_length,
             pad_to_multiple_of=self.pad_to_multiple_of,
             return_tensors="pt",
         )
-        # for val in batch['input_values']:
-        #   print(val[:10])
-        #   print(val[-10:])
-        # print(batch['input_values'].shape)
-        batch["labels"] = torch.tensor(output_features)
-        # print(batch["labels"].argmax(-1))
+        with self.processor.as_target_processor():
+            labels_batch = self.processor.pad(
+                label_features,
+                padding=self.padding,
+                max_length=self.max_length_labels,
+                pad_to_multiple_of=self.pad_to_multiple_of_labels,
+                return_tensors="pt",
+            )
+
+        # replace padding with -100 to ignore loss correctly
+        labels = labels_batch["input_ids"].masked_fill(
+            labels_batch.attention_mask.ne(1), -100
+        )
+
+        batch["labels"] = labels
         return batch
